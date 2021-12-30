6863ce21f516ad50d9ad36db6d236f95f58c299b
jonsanta <16006521+santajon@users.noreply.github.com>
2021-12-30 01:29:25 +0100

    project animations improved, project images improved

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -1,36 +1,34 @@
-import { trigger, state, style} from '@angular/animations';
 import { Component, OnInit, Input} from '@angular/core';
 
 @Component({
   selector: 'project',
   templateUrl: './project.component.html',
-  styleUrls: ['./project.component.css'],
-  animations:[
-    trigger('info', [
-      state('close', style({
-        'height': '0px',
-        'color': 'transparent',
-        'pointer-events': 'none',
-        'transition': 'height 200ms ease-in-out, color 100ms ease'
-      })),
-      state('show', style({
-        'height': '220px',
-        'color': 'black',
-        'pointer-events': 'auto',
-        'transition': 'height 200ms ease-in-out, color 200ms cubic-bezier(0,0,0,-1)'
-      }))
-    ]),
-  ]
+  styleUrls: ['./project.component.css']
 })
 export class ProjectComponent implements OnInit {
 
   @Input() url : string;
+  @Input("ts") initialTransformScale : string;
+  @Input("ats") animationTransformScale : string;
+  trfS : string;
 
   constructor() { 
     this.url = "";
+    this.initialTransformScale = "scale(1)";
+    this.trfS = this.initialTransformScale;
+    this.animationTransformScale = "scale(1.5)";
   }
 
   ngOnInit(): void {
+    this.trfS = this.initialTransformScale;
+  }
+
+  show(): void{
+    this.trfS = this.animationTransformScale;
+  }
+
+  close(): void{
+    this.trfS = this.initialTransformScale;
   }
 
 }
