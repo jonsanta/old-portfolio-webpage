bce56e351e94c06d5f9f21ff4b936551ebe8ed87
jonsanta <jon.santamaria1995@gmail.com>
2022-02-14 21:05:37 +0100

    Update project.component.ts

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input} from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'project',
@@ -13,12 +14,15 @@ export class ProjectComponent implements OnInit {
   @Input("desc") description : string;
   @Input("repo") repository : string;
   @Input("pre") preview : string;
+  @Input("vsrc") videoSRC : string;
+  showVideo : boolean;
+  safeSrc: SafeResourceUrl;
 
   @Input() buttonText : string[] = [];
 
   property : string;
 
-  constructor() {
+  constructor(private sanitizer: DomSanitizer) {
     this.url = "";
     this.property = "";
     this.backgroundMode = "cover";
@@ -27,14 +31,24 @@ export class ProjectComponent implements OnInit {
     this.repository ="";
     this.preview ="";
     this.buttonText = ["Preview", "Repository"];
+    this.showVideo = false;
+    this.videoSRC = "";
+    this.safeSrc = this.sanitizer.bypassSecurityTrustResourceUrl("");
+    
   }
 
   ngOnInit(): void {
       this.property = "url("+this.url+")";
+      this.safeSrc = this.sanitizer.bypassSecurityTrustResourceUrl(this.videoSRC);
   }
 
   onClick(option : string): void{
-    if(option == "repo") window.open("https://github.com/"+this.repository, "_blank");
-    else if(option == "pre") window.open(this.preview, "_blank");
+    if(option == "repo") window.open("https://github.com/jonsanta/"+this.repository, "_blank");
+    else if(option == "pre" && this.videoSRC == "") window.open(this.preview, "_blank");
+    else if(option == "pre" && this.videoSRC != "") this.showVideo = true;
+  }
+
+  closeVideo(): void{
+    this.showVideo = false;
   }
 }
