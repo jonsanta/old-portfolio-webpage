d03b356d41ce99ee482a730d3f76359e8eee789a
jonsanta <jon.santamaria1995@gmail.com>
2022-02-15 20:54:59 +0100

    Update left-nav-bar-item.component.ts

--- src/app/nav-bar/navbar-sections/left-nav-bar-item/left-nav-bar-item.component.ts
+++ src/app/nav-bar/navbar-sections/left-nav-bar-item/left-nav-bar-item.component.ts
@@ -1,18 +1,44 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { trigger, state, style} from '@angular/animations';
+import { Component, OnInit, OnChanges, Input, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'left-nav-item',
   templateUrl: './left-nav-bar-item.component.html',
-  styleUrls: ['./left-nav-bar-item.component.css']
+  styleUrls: ['./left-nav-bar-item.component.css'],
+  animations:[
+    trigger('sectionScoped', [
+      state('black', style({
+      })),
+      state('grey', style({
+        'color': '#C0C0C0'
+      }))
+    ]),
+  ]
 })
-export class LeftNavBarItemComponent implements OnInit {
+export class LeftNavBarItemComponent implements OnInit, OnChanges {
 
   @Input() content : string;
+  @Input() index : string;
+  @Input() givenIndex : number;
+
+  scoped : boolean;
 
   constructor() { 
     this.content = "";
+    this.index = "";
+    this.givenIndex = 0;
+    this.scoped = false;
   }
 
   ngOnInit(): void {
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+      if(parseInt(this.index) == this.givenIndex){
+        this.scoped = true;
+      }
+      else{
+        this.scoped = false;
+      }
   }
 }
