139bd8cb9c98badc99eab48927aad5a42fdbff13
jonsanta <jon.santamaria1995@gmail.com>
2022-02-15 20:51:51 +0100

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,6 +15,7 @@ export class AppComponent implements OnInit {
   buttonText : string[] = [];
   footerText : string = "Developed and designed by Jon Santamaria Zamora.";
 
+  selectedSection : number = 0;
 
   constructor()
   {
@@ -27,6 +28,7 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     this.detectMobile();
+    this.getSection();
   }
 
   changeLanguage() : void {
@@ -35,6 +37,21 @@ export class AppComponent implements OnInit {
     this.contact = Lang.getContact();
     this.buttonText = Lang.getProjectButtons();
     this.footerText = Lang.getFooter();
+  }
+
+  @HostListener('window:scroll', ['$event']) // window scroll event
+  getSection() : void{
+    let sections : string[] = ["home", "aboutme", "projects", "contact"];
+    for(let x = 0; x < sections.length; x++)
+    {
+      let elementRect = document.getElementById(sections[x])!.getBoundingClientRect();
+      if(elementRect.top >= -200 && elementRect.top <= 150)
+      {
+        this.selectedSection = x; //highlight the spotted section name on navbar
+        if(document.getElementById(sections[3])!.getBoundingClientRect().top < 620)
+          this.selectedSection = 3; //force contact to highlight
+      }
+    }
   }
 
   @HostListener('window:resize', ['$event'])
